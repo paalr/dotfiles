af718c1dd532545b81098044e13f7658ac5b0bb8
paalr <pal@roaldsoy.com>
2026-03-03 09:17:54 +0100

    Add Ruby installation script and update colorls setup

---
+++ ruby/install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Ruby - install modern Ruby version via rbenv
+
+if test ! $(which rbenv)
+then
+  echo "  Installing rbenv for you."
+  brew install rbenv > /tmp/rbenv-install.log
+fi
+
+# Get the Ruby version from .ruby-version file
+RUBY_VERSION=$(cat $HOME/.dotfiles/.ruby-version)
+
+# Check if the specified Ruby version is installed
+if ! rbenv versions | grep -q "$RUBY_VERSION"; then
+  echo "  Installing Ruby $RUBY_VERSION..."
+  rbenv install $RUBY_VERSION
+  rbenv global $RUBY_VERSION
+  echo "  Ruby $RUBY_VERSION installed and set as global version"
+else
+  echo "  Ruby $RUBY_VERSION is already installed"
+  rbenv global $RUBY_VERSION
+fi
+
+# Rehash rbenv
+rbenv rehash
+
+echo "  Current Ruby version: $(ruby -v)"
